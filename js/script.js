330286d26a8e6db3b0df5a199687ce64ba0c3cb0
Antares-ProgramIng <140986762+Antares-ProgramIng@users.noreply.github.com>
2023-08-07 21:25:34 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -10,12 +10,46 @@ const container = document.getElementById("container"); // "Traemos" utilizando 
  * Los datos se mostrarán dentro del div de id "container" y por cada ítem se está creando un nuevo párrafo donde se
  * imprime el campo "name" y el campo "lastname" separados por un espacio
  */
+
+
 function showData(dataArray) {
   // El for itera sobre los elementos del array
-  for (const item of dataArray) {
+  for (const item of dataArray.students) {
     // En la siguiente línea se utilizan "backticks" para armar el String. Más info => https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Template_literals
     container.innerHTML += `<p> ${item.name} ${item.lastname} </p>`; // Se concatena cada párrafo de la manera que queremos mostrarlo al innerHTML del contenedor
   }
 }
 
+let getJSONData = function(url){
+  let result = {};
+  return fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }else{
+      throw Error(response.statusText);
+    }
+  })
+  .then(function(response) {
+        result.status = 'ok';
+        result.data = response;
+        return result;
+  })
+  .catch(function(error) {
+      result.status = 'error';
+      result.data = error;
+      return result;
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+  getJSONData(DATA_URL).then(function(resultObj){
+      if (resultObj.status == "ok"){
+          dataArray = resultObj.data
+          showData(dataArray)
+      };
+    });
+  });
+
+  
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
